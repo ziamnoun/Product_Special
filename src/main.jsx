ae4b0426aa5fcbd777a1df9fce28170d1726cdc9
ziamnoun <ziamziam05@gmail.com>
2024-08-19 20:25:33 +0600

    TEst

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!bye</div>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router} />
+  </StrictMode>,
+)
